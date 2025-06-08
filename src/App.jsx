dcce701c94aae4a343c260eef56477995f8a2661
kanha charan dash <kanhacharandash111@gmail.com>
2025-06-08 20:57:30 +0530

    adding react router ti the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-import "./App.css";
-// eslint-disable-next-line import/extensions
+import { Route, Switch, NavLink } from "react-router-dom";
+
+import Home from "./Components/Home";
+import PageNotFound from "./Components/PageNotFound ";
 import Product from "./Components/Product";
 
 const App = () => (
-  <div className="App">
-    <Product />
-  </div>
+  <>
+    <NavLink exact activeClassName="underline font-bold" to="/">
+      Home
+    </NavLink>
+    <NavLink exact activeClassName="underline font-bold" to="/product">
+      Product
+    </NavLink>
+    <Switch>
+      <Route exact component={Home} path="/" />
+      <Route exact component={Product} path="/product" />
+      <Route component={PageNotFound} path="*" />
+    </Switch>
+  </>
 );
 
 export default App;

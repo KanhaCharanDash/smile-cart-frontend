5f010612b1018b0c84cf66134621e4cef5c86b46
kanha charan dash <kanhacharandash111@gmail.com>
2025-06-08 15:58:29 +0530

    Implemented Axios interceptors to set http headers and to transform response

---
+++ src/apis/axios.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+import { keysToCamelCase } from "neetocist";
+
+const transformResponseKeysToCamelCase = response => {
+  if (response.data) response.data = keysToCamelCase(response.data);
+};
+
+const responseInterceptors = () => {
+  axios.interceptors.response.use(response => {
+    transformResponseKeysToCamelCase(response);
+
+    return response.data;
+  });
+};
+
+// Create a function to configure the default HTTP headers as follows.
+const setHttpHeaders = () => {
+  axios.defaults.headers = {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  };
+};
+// To avoid the repetition of specifying this URL in every request in the codebase, Axios provides a convenient feature that allows us to set a default baseURL, which can be achieved like this:
+
+// axios.defaults.baseURL =
+//   "https://smile-cart-backend-staging.neetodeployapp.com/";
+
+export default function initializeAxios() {
+  axios.defaults.baseURL =
+    "https://smile-cart-backend-staging.neetodeployapp.com/";
+  setHttpHeaders();
+  responseInterceptors();
+}
